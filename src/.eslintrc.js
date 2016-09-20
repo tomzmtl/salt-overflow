fcd5d626824d17edace5f92ae0191578d2d09c26
Thomas <thomas.andreo@gmail.com>
2016-09-19 23:45:35 -0400

    Added react.

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -3,4 +3,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 6,
   },
+  env: {
+    browser: true,
+  },
 };
--- src/client/app.js
+++ src/client/app.js
@@ -1,1 +1,14 @@
+/* eslint-disable react/jsx-filename-extension */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import '../scss/app.scss';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <div>Hola amigo!</div>,
+    document.getElementById('root')
+  );
+});
