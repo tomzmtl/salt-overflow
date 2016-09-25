9e07a70cf6599774b9079ff0674814cba6dff5c8
Thomas Andreo <thomas.andreo@gmail.com>
2016-09-25 00:09:39 -0400

    Fixed API, made payload spec-compliant.

--- src/server/api/players.js
+++ src/server/api/players.js
@@ -1,17 +1,27 @@
 import { MongoClient } from 'mongodb';
+import dotEnv from 'dotenv';
 
 
 export default null;
 
 
 export const all = (req, res) => {
+  dotEnv.config();
+
   // Connection URL
   const url = process.env.MONGODB_URI;
 
   // Use connect method to connect to the server
   MongoClient.connect(url, (err, db) => {
     const collection = db.collection('players');
-    collection.find().toArray().then((r) => { res.send(r); });
+    collection.find().toArray().then((r) => {
+      res.send(Object.assign(r, {
+        played_characters: [],
+        win_percent: 0,
+        games_played: 0,
+        sessions_played: 0,
+      }));
+    });
     db.close();
   });
 };
