8f61fcea5f26e09c34e536a4217dc62561772174
Thomas Andreo <thomas.andreo@gmail.com>
2016-09-25 00:40:07 -0400

    Removed _id from API payload

--- src/server/api/players.js
+++ src/server/api/players.js
@@ -1,5 +1,6 @@
 import { MongoClient } from 'mongodb';
 import dotEnv from 'dotenv';
+import omit from 'lodash.omit';
 
 
 export default null;
@@ -15,12 +16,14 @@ export const all = (req, res) => {
   MongoClient.connect(url, (err, db) => {
     const collection = db.collection('players');
     collection.find().toArray().then((r) => {
-      res.send(r.map(row => Object.assign(row, {
-        played_characters: [],
-        win_percent: 0,
-        games_played: 0,
-        sessions_played: 0,
-      })));
+      res.send(r.map((row) => {
+        return omit(Object.assign(row, {
+          played_characters: [],
+          win_percent: 0,
+          games_played: 0,
+          sessions_played: 0,
+        }), ['_id']);
+      }));
     });
     db.close();
   });
